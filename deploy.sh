9ce0c92ac5550418702b3512649e562b1c16d90a
Thiago Freitas <thiagosistemas3@gmail.com>
2023-02-05 11:17:29 +0000

    a

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+echo $1
+
 git pull
 sudo docker stop $(sudo docker ps | grep "akaytatsu/iec-fin-front" | cut -d " " -f 1)
-docker run -d -p 7054:80 akaytatsu/iec-fin-front:'$1'
+docker run -d -p 7054:80 akaytatsu/iec-fin-front:$1
