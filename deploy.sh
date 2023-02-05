4b7967594a6de918e4be4ed9bb4651218621b2c8
Thiago Freitas <thiagosistemas3@gmail.com>
2023-02-05 11:31:45 +0000

    a

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo $1
 
 git pull
-sudo docker stop $(sudo docker ps | grep "akaytatsu/iec-fin-front" | cut -d " " -f 1)
-docker run -d -p 7054:80 akaytatsu/iec-fin-front:$1
+docker stop $(docker ps | grep "akaytatsu/iec-fin-front" | cut -d " " -f 1) || echo "No container to stop"
+docker run -d -p 7054:80 akaytatsu/iec-fin-front:$1 || echo "Failed to start container"
